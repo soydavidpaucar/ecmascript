3a4dcd4289988d85322204312ebacec92c81b341
David Paucar <soydavidpaucar@gmail.com>
2021-03-08 22:25:52 -0500

    :sparkles: flat map
    se creo el codigo con el funcionamiento de flat map en arrays, a partir de es10

--- src/ecmascript-10/index.js
+++ src/ecmascript-10/index.js
@@ -3,3 +3,8 @@ let array = [1, 2, 3, [1, 2, 3, [1, 2, 3]]];
 /* GENERACION DE UNA NUEVA MATRIZ DEPENDIENDO DE LA PROFUNDIDAD DE LOS SUBNIVELES DEL ARRAY */
 console.log(array.flat(2));
 
+/* FLAT MAP */
+let arreglo = [1, 2, 3, 4, 5];
+/* MAPEAR CADA ELEMENTO, LUEGO PASARLE UNA FUNCION Y APLANAR */
+console.log(arreglo.flatMap(value => [value, value * 2]));
+
