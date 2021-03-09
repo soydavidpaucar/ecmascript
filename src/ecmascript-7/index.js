01c5c2e338dffec5eb30356b006ec6b105b17fc4
David Paucar <soydavidpaucar@gmail.com>
2021-03-08 20:55:33 -0500

    :pencil2: typo dentro de includes
    se arreglo el typo dentro de la funcion includes

--- src/ecmascript-7/index.js
+++ src/ecmascript-7/index.js
@@ -1,7 +1,7 @@
 /* INCLUDES A PARTIR DE ES7 */
 let numbers = [1, 2, 3, 7, 8];
 
-if (numbers.includes(7, 3)) {
+if (numbers.includes(7)) {
 	console.log('Si Hay Un 7');
 } else {
 	console.log('No Se Encuentra');
