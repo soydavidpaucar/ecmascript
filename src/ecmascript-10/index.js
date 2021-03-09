a5c769e67b98d9acf386ace55c72f76519e9fe43
David Paucar <soydavidpaucar@gmail.com>
2021-03-08 22:19:45 -0500

    :sparkles: flat arrays
    se creo el codigo con el funcionamiento del metodo flat en arrays, a partir de es10

---
+++ src/ecmascript-10/index.js
@@ -0,0 +1,5 @@
+/* FLAT EN ARRAYS */
+let array = [1, 2, 3, [1, 2, 3, [1, 2, 3]]];
+/* GENERACION DE UNA NUEVA MATRIZ DEPENDIENDO DE LA PROFUNDIDAD DE LOS SUBNIVELES DEL ARRAY */
+console.log(array.flat(2));
+
