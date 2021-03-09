6523480e931161875d871a2671b8329d57f7ceac
David Paucar <soydavidpaucar@gmail.com>
2021-03-08 22:03:30 -0500

    :sparkles: promises finally
    se creo el codigo con el funcionamiento de promises finally, a partir de es9

--- src/ecmascript-9/index.js
+++ src/ecmascript-9/index.js
@@ -22,3 +22,18 @@ const objecto1 = {
 
 console.log(objecto1);
 
+/* PROMISE FINALLY */
+const helloWorld = () => {
+	return new Promise((resolve, reject) => {
+		(true)
+		? setTimeout(() => resolve('Hello World'), 3000)
+		: reject(new Error('Test Error'));
+	});
+};
+
+helloWorld()
+.then(response => console.log(response))
+.catch(error => console.log(error))
+.finally(() => console.log('Finalizo'));
+
+
