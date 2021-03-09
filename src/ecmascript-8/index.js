8fc2ff163267278cf0d2db8a0b38ee0661bf30de
David Paucar <soydavidpaucar@gmail.com>
2021-03-08 21:07:11 -0500

    :sparkles: object values
    se creo el codigo con el funcionamiento de los object values, a partir de es8

--- src/ecmascript-8/index.js
+++ src/ecmascript-8/index.js
@@ -10,3 +10,14 @@ console.log(entries);
 /* VERIFICA CUANTOS ELEMENTOS HAY EN LA MATRIZ */
 console.log(entries.length);
 
+/* OBJECT VALUES, DEVUELVE LOS VALORES DE UN OBJETO A UN ARREGLO */
+const datos = {
+	frontend: 'David',
+	backend : 'Isabel',
+	design  : 'Ana',
+};
+const valores = Object.values(datos);
+console.log(valores);
+/* DEVUELVE LA CANTIDAD DE ELEMENTOS DEL ARREGLO */
+console.log(valores.length);
+
